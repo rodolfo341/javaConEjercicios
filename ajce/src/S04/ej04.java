ac2c507f76b3ece1cb03cd6854d66b68bab1db58
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-15 19:28:00 -0300

    dia 1 15/12/23 salida por pantalla ej 4

---
+++ ajce/src/S04/ej04.java
@@ -0,0 +1,17 @@
+package S04;
+/**
+* 1. Salida por pantalla
+*
+* 4. Escribe un programa que muestre tu horario de clase. Puedes usar
+* espacios o tabuladores para alinear el texto.
+*
+* @author Rodolfo aravena
+*/
+public class ej04 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
