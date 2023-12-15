d818fdb3a73f1834371957d857f38ff46ef19281
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-15 18:34:02 -0300

    dia 1 15/12/23 salida por pantalla ej 1

---
+++ ajce/src/S01/ej01.java
@@ -0,0 +1,30 @@
+package S01;
+
+import java.util.Scanner;
+
+/**
+* 1. Salida por pantalla
+*
+* 1. Escribe una programa que muestre tu nombre por pantalla.
+*
+* @author Rodolfo Aravena
+*/
+
+public class ej01 {
+
+	public static void main(String[] args) {		
+			System.out.println("Rodolfo Aravena");
+			
+	}
+
+}
+
+/*
+Scanner sc = new Scanner(System.in);
+char res;
+
+do {
+	
+	System.out.print("¿Desea continuar? (S/N): "); res = sc.next().charAt(0);
+} while (Character.toUpperCase(res) == 'S');
+*/
