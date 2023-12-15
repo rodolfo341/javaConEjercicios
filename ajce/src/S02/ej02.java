282b37a9377026cea5fe90c367be070ac185428f
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-15 19:20:00 -0300

    dia 1 15/12/23 salida por pantalla ej 3

--- ajce/src/S02/ej02.java
+++ ajce/src/S02/ej02.java
@@ -1,10 +1,21 @@
 package S02;
 
+/**
+* 1. Salida por pantalla
+*
+* 2. Modifica el programa anterior para que además se muestre tu
+* dirección y tu número de teléfono. Asegúrate de que los
+* datos se muestran en líneas separadas.
+*
+* @author Rodolfo Aravena
+*/
+
 public class ej02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Rodolfo Aravena");
+		System.out.println("Siempre Viva 543");
+		System.out.println("98765321");
 	}
 
 }
---
+++ ajce/src/S03/ej03.java
@@ -0,0 +1,27 @@
+package S03;
+/**
+* 1. Salida por pantalla.
+*
+* Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+* correspondiente traducción al castellano. Las palabras deben estar distribuidas
+* en dos columnas y alineadas a la izquierda.
+*
+*
+* @author Luis José Sánchez
+*/
+
+public class ej03 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello:\t Hola");
+		System.out.println("Goodbye: Adiós");
+		System.out.println("Friend:\t Amigo");
+		System.out.println("Family:\t Familia");
+		System.out.println("Love:\t Amor");
+		System.out.println("Home:\t Hogar");
+		System.out.println("Time:\t Tiempo");
+		System.out.println("Work:\t Trabajo");
+		System.out.println("School:\t Escuela");
+		System.out.println("Day:\t Día");		
+	}
+}
