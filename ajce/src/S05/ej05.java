b6bf09daa283821f94bbfab0097f42261cabaf3e
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-15 21:46:35 -0300

    dia 1 15/12/23 salida por pantalla ej 5

---
+++ ajce/src/S05/ej05.java
@@ -0,0 +1,17 @@
+package S05;
+/**
+* 5. Modifica el programa anterior añadiendo colores. Puedes mostrar
+* cada asignatura de un color diferente.
+*
+* @author Rodolfo Aravena
+*/
+public class ej05 {
+
+	public static void main(String[] args) {
+		System.out.println("LUNES \t\t MARTES \t MIERCOLES");
+		System.out.println("-------------------------------------------");
+		System.out.println("desayuno \t desayuno \t desayuno");
+		System.out.println("cena \t\t cena \t\t cena");
+	}
+
+}
