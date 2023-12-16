b62445064ddb511d2d4ea4840b749bfeb0aee3f5
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-15 22:26:17 -0300

    dia 1 15/12/23 salida por pantalla ej 6

--- ajce/src/S06/ej06.java
+++ ajce/src/S06/ej06.java
@@ -12,7 +12,17 @@ package S06;
 public class ej06 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("             *");
+		System.out.println("            * *");
+		System.out.println("           *   *");
+		System.out.println("          *     *");
+		System.out.println("         *       *");
+		System.out.println("        *         *");
+		System.out.println("       *           *");
+		System.out.println("      *             *");
+		System.out.println("     * * * * * * * * * ");
 
 	}
 
