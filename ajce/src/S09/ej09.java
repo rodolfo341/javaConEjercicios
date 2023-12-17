3e769b46521b96cfa3ae6c3dcfd673c6466c1964
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-17 16:02:10 -0300

    dia 6 15/12/23 salida por pantalla ej 9

---
+++ ajce/src/S09/ej09.java
@@ -0,0 +1,20 @@
+package S09;
+/**
+ * 
+ * 9. Escribe un programa que pinte por pantalla alguna escena - el campo,
+ * la habitación de una casa, un aula, etc. - o algún objeto animado o
+ * inanimado - un coche, un gato, una taza de café, etc. Ten en cuenta
+ * que puedes utilizar caracteres como *, +, <, #, @, etc. o incluso
+ * caracteres Unicode. ¡Échale imaginación!
+ *
+ * @author Rodolfo Aravena
+ *
+ */
+public class ej09 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
