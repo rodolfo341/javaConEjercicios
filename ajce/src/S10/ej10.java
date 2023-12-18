9682c223b67e42de01556afcb09b29881efa05f0
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-17 22:51:48 -0300

    dia 6 15/12/23 salida por pantalla ej 10

--- ajce/src/S10/ej10.java
+++ ajce/src/S10/ej10.java
@@ -1,16 +1,15 @@
-package S09;
+package S10;
 /**
- * 
- * 9. Escribe un programa que pinte por pantalla alguna escena - el campo,
+ * * 9. Escribe un programa que pinte por pantalla alguna escena - el campo,
  * la habitación de una casa, un aula, etc. - o algún objeto animado o
  * inanimado - un coche, un gato, una taza de café, etc. Ten en cuenta
  * que puedes utilizar caracteres como *, +, <, #, @, etc. o incluso
- * caracteres Unicode. ¡Échale imaginación!
- *
+ * caracteres Unicode. ¡Échale imaginación
+ * 
  * @author Rodolfo Aravena
- *
+ * 
  */
-public class ej09 {
+public class ej10 {
 
 	public static void main(String[] args) {
 		System.out.println("\n\n");
