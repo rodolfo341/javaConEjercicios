17fe5ad45bd1d421fa0cd546cad357c04d017667
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-18 16:37:00 -0300

    dia 8 18/12/23 ej06

--- ajce/src/S03/ej05.java
+++ ajce/src/S03/ej05.java
@@ -12,9 +12,16 @@ public class ej05 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		char res;
-						
-		do {
+		int alto;
+		int ancho;
+		int area;
 		
+		do {
+			System.out.println("Alto = "); alto = sc.nextInt();
+			System.out.println("Ancho = "); ancho = sc.nextInt();
+			area = alto * ancho;
+			System.out.println("El area es: " + area);
+			
 			System.out.print("¿Desea continuar? (S/N): "); res = sc.next().charAt(0);
 		} while (Character.toUpperCase(res) == 'S');
 		
