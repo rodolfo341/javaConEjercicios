912db6b677c0115532a2e9e9c8a8cc18caf6f0e7
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-18 10:53:18 -0300

    dia 7 18/12/23 lectura de datos preparacion

---
+++ ajce/src/S03/ej01.java
@@ -0,0 +1,10 @@
+package S03;
+
+public class ej01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ajce/src/S03/ej02.java
@@ -0,0 +1,10 @@
+package S03;
+
+public class ej02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
